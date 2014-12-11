eb091a0b583419724d3c2dcf6b2dd88ca0f79237
Aaron Weiss <aaronweiss74@gmail.com>
2014-12-11 00:06:12 -0500

    Simplified tweeter example code.

--- examples/tweeter.rs
+++ examples/tweeter.rs
@@ -19,18 +19,10 @@ fn main() {
     let irc_server = Arc::new(IrcServer::from_config(config).unwrap());
     let irc_server2 = irc_server.clone();
     // The wrapper provides us with methods like send_privmsg(...) and identify(...)
-    let server = Wrapper::new(&*irc_server);
+    let server = Wrapper::new(&*irc_server2);
     server.identify().unwrap();
-    // Let's set up a loop that ignores timeouts, and reads perpetually.
-    // n.b. this shouldn't exit automatically if the connection closes. 
-    spawn(proc() { 
-        let mut iter = irc_server2.iter();
-        loop {
-            if let Some(msg) = iter.next() {
-                print!("{}", msg.into_string());
-            }
-        }
-    });
+    // Let's set up a loop that just prints the messages.
+    spawn(proc() { irc_server.iter().map(|m| print!("{}", m.into_string())).count(); });
     loop {
         server.send_privmsg("#vana", "TWEET TWEET").unwrap();
         sleep(Duration::seconds(10))
