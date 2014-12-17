05a5efa6bb4be61b510da89c21233325e3b23157
Aaron Weiss <aaronweiss74@gmail.com>
2014-12-17 14:50:42 -0500

    Updated documentation.

--- implementors/core/ops/trait.Shl.js
+++ implementors/core/ops/trait.Shl.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['openssl'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Shl.html' title='core::ops::Shl'>Shl</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.i32.html'>i32</a>, <a class='struct' href='openssl/bn/struct.BigNum.html' title='openssl::bn::BigNum'>BigNum</a>&gt; for <a class='struct' href='openssl/bn/struct.BigNum.html' title='openssl::bn::BigNum'>BigNum</a>",];
+implementors['openssl'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Shl.html' title='core::ops::Shl'>Shl</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.i32.html'>i32</a>, <a class='struct' href='openssl/bn/struct.BigNum.html' title='openssl::bn::BigNum'>BigNum</a>&gt; for &amp;'a <a class='struct' href='openssl/bn/struct.BigNum.html' title='openssl::bn::BigNum'>BigNum</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
