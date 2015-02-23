1828c16766a158bf881d0ca1d5ae1a479ad0a000
Aaron Weiss <aaronweiss74@gmail.com>
2015-02-22 21:18:57 -0500

    Added a client prelude.

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -4,3 +4,13 @@
 pub mod conn;
 pub mod data;
 pub mod server;
+
+pub mod prelude {
+    //! A client-side IRC prelude, re-exporting all the necessary basics.
+    #![unstable = "Prelude is newly added, and contents have not yet firmed up."]
+
+    pub use client::server::{IrcServer, Server};
+    pub use client::server::utils::Wrapper;
+    pub use client::data::{Command, Config, Message, Response};
+    pub use client::data::kinds::{IrcReader, IrcWriter};
+}
