f6a4a9eb2608d3bb1792180118f37d95eee950d3
Aaron Weiss <aaronweiss74@gmail.com>
2015-03-02 17:35:52 -0500

    Rebuilt docs (but I had to comment out the CAP command).

---
+++ implementors/core/error/trait.FromError.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['rustc-serialize'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/error/trait.FromError.html' title='core::error::FromError'>FromError</a>&lt;<a class='struct' href='http://doc.rust-lang.org/nightly/core/fmt/struct.Error.html' title='core::fmt::Error'>Error</a>&gt; for <a class='enum' href='rustc-serialize/json/enum.EncoderError.html' title='rustc-serialize::json::EncoderError'>EncoderError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
--- implementors/core/hash/trait.Hash.js
+++ implementors/core/hash/trait.Hash.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['openssl'] = ["<a class='stability Unstable' title='Unstable: module was recently redesigned'></a>impl&lt;__S: <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Writer.html' title='core::hash::Writer'>Writer</a> + <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Hasher.html' title='core::hash::Hasher'>Hasher</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a>&lt;__S&gt; for <a class='enum' href='openssl/ssl/enum.SslMethod.html' title='openssl::ssl::SslMethod'>SslMethod</a>",];
+implementors['openssl'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='enum' href='openssl/ssl/enum.SslMethod.html' title='openssl::ssl::SslMethod'>SslMethod</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
