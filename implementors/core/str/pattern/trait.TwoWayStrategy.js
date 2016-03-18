64abc8b209d800653e10b36bf8d26322afd4ef61
Aaron Weiss <aaronweiss74@gmail.com>
2016-03-17 21:50:03 -0400

    Rebuilt docs.

--- implementors/core/str/pattern/trait.TwoWayStrategy.js
+++ implementors/core/str/pattern/trait.TwoWayStrategy.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libc'] = [];implementors['openssl'] = [];
+implementors['libc'] = [];implementors['bitflags'] = [];implementors['openssl'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
