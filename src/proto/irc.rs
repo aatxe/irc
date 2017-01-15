a6303d485853d2fc1994c5038079d18252094222
Aaron Weiss <aaronweiss74@gmail.com>
2017-01-15 17:02:37 -0500

    Removed dependence on tokio-proto.

--- src/proto/irc.rs
+++ src/proto/irc.rs
@@ -1,10 +1,9 @@
-//! Implementation of IRC protocol for Tokio.
+//! Implementation of IRC codec for Tokio.
 
 use std::io;
 use proto::line::LineCodec;
 use proto::message::Message;
-use tokio_core::io::{Codec, EasyBuf, Framed, Io};
-use tokio_proto::pipeline::{ServerProto, ClientProto};
+use tokio_core::io::{Codec, EasyBuf};
 
 /// An IRC codec built around an inner codec.
 pub struct IrcCodec<C: Codec> {
@@ -39,41 +38,5 @@ impl<C> Codec for IrcCodec<C> where C: Codec<In = String, Out = String> {
 
     fn encode(&mut self, msg: Message, buf: &mut Vec<u8>) -> io::Result<()> {
         self.inner.encode(msg.to_string(), buf)
-    }
-}
-
-/// Implementation of the IRC protocol backed by a line-delimited codec.
-pub struct IrcProto {
-    encoding_label: String,
-}
-
-impl IrcProto {
-    /// Creates a new IrcProto using the specified WHATWG encoding label.
-    fn new(label: &str) -> IrcProto {
-        IrcProto { encoding_label: label.to_owned() }
-    }
-}
-
-impl<T> ClientProto<T> for IrcProto where T: Io + 'static {
-    type Request = Message;
-    type Response = Message;
-
-    type Transport = Framed<T, IrcCodec<LineCodec>>;
-    type BindTransport = Result<Self::Transport, io::Error>;
-
-    fn bind_transport(&self, io: T) -> Self::BindTransport {
-        Ok(io.framed(try!(IrcCodec::new(&self.encoding_label))))
-    }
-}
-
-impl<T> ServerProto<T> for IrcProto where T: Io + 'static {
-    type Request = Message;
-    type Response = Message;
-
-    type Transport = Framed<T, IrcCodec<LineCodec>>;
-    type BindTransport = Result<Self::Transport, io::Error>;
-
-    fn bind_transport(&self, io: T) -> Self::BindTransport {
-        Ok(io.framed(try!(IrcCodec::new(&self.encoding_label))))
     }
 }
