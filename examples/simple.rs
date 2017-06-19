36eeca7db6c0687dedd0ae2c1e244b9b9ec0cd8d
Aaron Weiss <awe@pdgn.co>
2017-06-19 14:46:34 -0400

    Changed examples to point to `#irc-crate`.

--- examples/simple.rs
+++ examples/simple.rs
@@ -8,7 +8,7 @@ fn main() {
         nickname: Some("pickles".to_owned()),
         alt_nicks: Some(vec!["bananas".to_owned(), "apples".to_owned()]),
         server: Some("irc.fyrechat.net".to_owned()),
-        channels: Some(vec!["#vana".to_owned()]),
+        channels: Some(vec!["#irc-crate".to_owned()]),
         ..Default::default()
     };
     let server = IrcServer::from_config(config).unwrap();
