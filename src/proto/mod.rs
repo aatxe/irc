1ca609331c0f20e38d64169ebb2192bf3eff7b04
Aaron Weiss <awe@pdgn.co>
2017-06-20 11:48:42 -0400

    Added reexports to the proto module.

--- src/proto/mod.rs
+++ src/proto/mod.rs
@@ -6,3 +6,9 @@ pub mod irc;
 pub mod line;
 pub mod message;
 pub mod response;
+
+pub use self::caps::{Capability, NegotiationVersion};
+pub use self::command::{BatchSubCommand, Command};
+pub use self::irc::IrcCodec;
+pub use self::message::Message;
+pub use self::response::Response;
