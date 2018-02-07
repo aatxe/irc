decfd0b9bcff2cdb7e23f93ac1e61af02261f25e
Aaron Weiss <awe@pdgn.co>
2018-02-07 12:13:43 +0100

    Noted inaccuracies of the is_error function for Response.

--- src/proto/response.rs
+++ src/proto/response.rs
@@ -408,6 +408,10 @@ make_response! {
 
 impl Response {
     /// Determines whether or not this response is an error response.
+    ///
+    /// This error consideration is according to RFC2812, but is rather simplistic. It considers all
+    /// response codes above 400 to be errors, which misclassifies some extensions (e.g. from IRCv3)
+    /// that add responses and errors both in the same range (typically 700s or 900s).
     pub fn is_error(&self) -> bool {
         *self as u16 >= 400
     }
