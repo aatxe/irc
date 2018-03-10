f1f63223e8c928aa5be8ffe9a611610d8f1667da
Aaron Weiss <awe@pdgn.co>
2018-03-10 15:03:06 +0100

    Minimized dependencies for irc-proto.

--- irc-proto/src/lib.rs
+++ irc-proto/src/lib.rs
@@ -2,31 +2,11 @@
 
 #![warn(missing_docs)]
 
-extern crate bufstream;
 extern crate bytes;
-extern crate chrono;
-#[macro_use]
-extern crate failure;
 extern crate encoding;
 #[macro_use]
-extern crate futures;
-#[macro_use]
-extern crate log;
-extern crate native_tls;
-extern crate serde;
-#[macro_use]
-extern crate serde_derive;
-#[cfg(feature = "json")]
-extern crate serde_json;
-#[cfg(feature = "yaml")]
-extern crate serde_yaml;
-extern crate tokio_core;
+extern crate failure;
 extern crate tokio_io;
-extern crate tokio_mockstream;
-extern crate tokio_timer;
-extern crate tokio_tls;
-#[cfg(feature = "toml")]
-extern crate toml;
 
 pub mod caps;
 pub mod chan;
