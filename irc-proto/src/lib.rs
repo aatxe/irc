229670c9f7ca82ab3d40d4f3664f69c33210672a
Aaron Weiss <awe@pdgn.co>
2018-03-10 18:10:40 +0100

    Put the tokio codec (IrcCodec) behind a `tokio` feature.

--- irc-proto/src/lib.rs
+++ irc-proto/src/lib.rs
@@ -2,17 +2,21 @@
 
 #![warn(missing_docs)]
 
+#[cfg(feature = "tokio")]
 extern crate bytes;
 extern crate encoding;
 #[macro_use]
 extern crate failure;
+#[cfg(feature = "tokio")]
 extern crate tokio_io;
 
 pub mod caps;
 pub mod chan;
 pub mod command;
 pub mod error;
+#[cfg(feature = "tokio")]
 pub mod irc;
+#[cfg(feature = "tokio")]
 pub mod line;
 pub mod message;
 pub mod mode;
@@ -21,6 +25,7 @@ pub mod response;
 pub use self::caps::{Capability, NegotiationVersion};
 pub use self::chan::ChannelExt;
 pub use self::command::{BatchSubCommand, CapSubCommand, Command};
+#[cfg(feature = "tokio")]
 pub use self::irc::IrcCodec;
 pub use self::message::Message;
 pub use self::mode::{ChannelMode, Mode, UserMode};
