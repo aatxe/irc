3870fe5ad73735b21659f232d3f3a94706ef2a69
snowpoke <hello@snowpoke.ink>
2022-11-14 17:05:24 +0100

    make unparsed message content public

--- no-parse-codec/src/lib.rs
+++ no-parse-codec/src/lib.rs
@@ -8,7 +8,7 @@ use irc_interface::{
 pub type NoParseCodec = LineCodec<UnparsedMessage>;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct UnparsedMessage(String);
+pub struct UnparsedMessage(pub String);
 
 impl Display for UnparsedMessage {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
